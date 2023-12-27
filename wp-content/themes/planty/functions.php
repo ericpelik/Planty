1f3859a49f996c28bb7355143409721b25fb1184
ericpelik <pleyber.eric@gmail.com>
2023-12-27 15:56:36 +0100

    ajout theme enfant Kadence V2

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -1,7 +1,13 @@
-<?php 
-
-add_action('wp_enqueue_script', 'theme_enqueue_styles');
-function theme_enqueue_styles()
-{
-    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+<?php
+/**
+ * Enqueue child styles.
+ */
+function child_enqueue_styles() {
+	wp_enqueue_style( 'child-theme', get_stylesheet_directory_uri() . '/style.css', array(), 100 );
 }
+
+// add_action( 'wp_enqueue_scripts', 'child_enqueue_styles' ); // Remove the // from the beginning of this line if you want the child theme style.css file to load on the front end of your site.
+
+/**
+ * Add custom functions here
+ */
