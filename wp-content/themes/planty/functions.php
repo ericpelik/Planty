15f03a4c2c4121661d1fecaa63e84505ba69ce36
ericpelik <pleyber.eric@gmail.com>
2023-12-28 08:56:40 +0100

    ajout fichier theme css et modification functions php

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -3,7 +3,8 @@
  * Enqueue child styles.
  */
 function child_enqueue_styles() {
-	wp_enqueue_style( 'child-theme', get_stylesheet_directory_uri() . '/style.css', array(), 100 );
+	wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
 
 // add_action( 'wp_enqueue_scripts', 'child_enqueue_styles' ); // Remove the // from the beginning of this line if you want the child theme style.css file to load on the front end of your site.
